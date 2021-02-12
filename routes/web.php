5fe6e966356f492eb63cbd85d122efb65ff66f7d
rosstims <78340242+rosstims@users.noreply.github.com>
2021-02-12 14:29:37 +0800

    by groupmates

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 | EDITED
 */
+// edited by groupmate
 
 Route::get('/', function () {
     return view('welcome');
